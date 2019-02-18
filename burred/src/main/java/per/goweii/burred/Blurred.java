4112c18a9c22717b3229438efa4d58a7a72acb26
goweii <goweii@163.com>
2019-02-18 16:03:48 +0800

    完善代码

--- burred/src/main/java/per/goweii/burred/Blurred.java
+++ burred/src/main/java/per/goweii/burred/Blurred.java
@@ -8,7 +8,6 @@ import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.view.View;
 
 import java.util.concurrent.ExecutorService;
